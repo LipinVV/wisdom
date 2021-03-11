d2d2489d74d4d320e3cecd9ce7075006a062801c
Ultra <vit.lipin@gmail.com>
2021-03-11 17:27:33 +0300

    added an option to select all words by one checkbox, made a draft for the line which describes how many words we have chosen

--- src/components/dictionary/dictionary.js
+++ src/components/dictionary/dictionary.js
@@ -11,7 +11,7 @@ export const Dictionary = () => {
 
     const [search, setSearch] = useState('');
 
-    const handleSearcher = (evt) => {
+    const handleSearcher = evt => {
         const { value } = evt.target;
         setSearch(value);
     }
@@ -29,19 +29,41 @@ export const Dictionary = () => {
         })
     }
 
+    const [globallyChecked, setGloballyChecked] = useState(false);
+
+    const globalHandleChanger = () => {
+        setGloballyChecked(prevState => !prevState)
+    }
+
+
+    const declineNoun = ([one, few, many]) => count => {
+        count = Math.abs(count) % 100;
+        let count1 = count % 10;
+        if (count > 10 && count < 20) { return many; }
+        if (count1 > 1 && count1 < 5) { return few; }
+        if (count1 == 1) { return one; }
+        return many;
+    }
+
     return (
         <div className='container'>
             <div className='dictionary'>
-                [показать кол-во совпадений, аудиофайл слов]
-            <h1 className='dictionary__header'>Ваш словарь</h1>
-
+                <h1 className='dictionary__header'>Ваш словарь</h1>
+                <label
+                    className={globallyChecked === false ? 'dictionary__check-all-words' : 'dictionary__check-all-words dictionary__check-all-words--active'}>Выбрать все слова из списка
+                    <input className='dictionary__global-checkbox'
+                        type='checkbox'
+                        value=''
+                        onChange={globalHandleChanger}
+                    />
+                </label>
+                {globallyChecked ? `Вы выбрали: ${dictionary.map(x => x).length}` : `Вы выбрали: ${checked.length}`}
                 <input
                     className='dictionary__input'
                     type='text'
                     placeholder='Поиск по слову...'
                     onChange={handleSearcher}
-                >
-                </input>
+                />
                 {dictionary
                     .filter((value) => {
                         if (search === '') {
@@ -76,10 +98,10 @@ export const Dictionary = () => {
                                     <input
                                         type='checkbox'
                                         value={word.id}
-                                        checked={checked.includes(word.id)}
+                                        checked={checked.includes(word.id) || globallyChecked}
                                         onChange={handleChanger}
-                                    >
-                                    </input>
+                                    />
+                                    {console.log(globallyChecked)}
                                 </li>
                                 <li className='dictionary__id'>{word.id}</li>
                                 <li className='dictionary__word'>{word.word}</li>
