af88d238a6fc1938c1343b014e440c5ed418e667
Ultra <vit.lipin@gmail.com>
2021-03-12 08:49:57 +0300

    added a function to help with word endings in case of changing numbers

--- src/components/dictionary/dictionary.js
+++ src/components/dictionary/dictionary.js
@@ -1,8 +1,6 @@
 import { React, useState } from 'react'
 import { dictionary } from './data'
 
-// https://firebase.google.com/ - для хостинга базы данных с аудио
-// https://vercel.com/ - хостинг для проекта
 // git init - инициация репо
 // git remote add origin git@github.com:LipinVV/chineese-app.git
 // 
@@ -36,28 +34,29 @@ export const Dictionary = () => {
     }
 
 
-    const declineNoun = ([one, few, many]) => count => {
+    const declineNoun = count => {
         count = Math.abs(count) % 100;
         let count1 = count % 10;
-        if (count > 10 && count < 20) { return many; }
-        if (count1 > 1 && count1 < 5) { return few; }
-        if (count1 == 1) { return one; }
-        return many;
+        if (count > 10 && count < 20) { return 'слов'; }
+        if (count1 > 1 && count1 < 5) { return 'слова'; }
+        if (count1 === 1) { return 'слово'; }
+        return 'слов';
     }
-
+    console.log(declineNoun)
     return (
         <div className='container'>
             <div className='dictionary'>
                 <h1 className='dictionary__header'>Ваш словарь</h1>
+                [нужна сортировка по русскому алфавиту; далее - опция по формированию из выбранных слов уникального набора слов для изучения]<br/>
+                [проблема №1: вводим любую букву и жмём "выбрать все" - выбираются все слова, вместо отрисованных]
                 <label
                     className={globallyChecked === false ? 'dictionary__check-all-words' : 'dictionary__check-all-words dictionary__check-all-words--active'}>Выбрать все слова из списка
                     <input className='dictionary__global-checkbox'
                         type='checkbox'
-                        value=''
                         onChange={globalHandleChanger}
                     />
                 </label>
-                {globallyChecked ? `Вы выбрали: ${dictionary.map(x => x).length}` : `Вы выбрали: ${checked.length}`}
+                {globallyChecked ? `Вы выбрали: ${dictionary.map(x => x).length} ${declineNoun(dictionary.map(x => x).length)}` : `Вы выбрали: ${checked.length} ${declineNoun(checked.length)}`}
                 <input
                     className='dictionary__input'
                     type='text'
